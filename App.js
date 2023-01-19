b32321064f69a759285d40f695ef76a4ffc693e5
prm1999 <mourya.18bcs1117@aes.ac.in>
2023-01-19 19:31:44 +0530

    app info

---
+++ App.js
@@ -0,0 +1,34 @@
+
+
+const HeaderComponent=()=>{
+
+}
+
+const AppLayout=()=>{
+    return(
+        /**
+         * Haeder 
+         * -Logo
+         * -Nav Item(Right Side)
+         * -Cart
+         * Body
+         * -Search Bar
+         * -Resturant List
+         * -- Resturant Card
+         * ---Image
+         * ---Name
+         * ---Rating
+         * ---Cusines
+         * Footer
+         * -Links
+         * -CopyRight
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         */
+    )
+}
--- index.js
+++ index.js
@@ -1,30 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React Element
-
-// const heading=React.createElement(
-//     "h1",
-//     {
-//         id:"title",
-//     },
-//     "Heading 1 for parcel"
-// );
-
-// const haeding2=React.createElement(
-//     "h2",
-//     {
-//         id:"title",
-//     },
-//     "Heading 2"
-// );
-// const container=React.createElement(
-//     "div",
-//     {
-//         id:"container",
-//     },
-//     [heading,haeding2],
-// );
 
 
 
